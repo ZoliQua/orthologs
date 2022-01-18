12693650ac4ded771f3db0e917ef334d9e64216f
Zoltán Dul <zoltan.dul@gmail.com>
2022-01-18 23:55:25 +0100

    update files

--- stringDB_variables.py
+++ stringDB_variables.py
@@ -1,3 +1,4 @@
+
 # Ortholog Parser / STRING DB Reader
 #
 # What this file do?
@@ -21,20 +22,20 @@ current_time_abbrev = now.strftime("%Y%m%d-%H%M%S-%f")
 taxon_list = ('9606', '7955', '6239', '3702', '7227', '4896', '4932')
 
 # Taxon dict taxid :: taxon_name
-taxon_dict = {'9606': 'H. sapiens',
-              '7955': 'D. rerio',
-              '6239': 'C. elegans',
-              '3702': 'A. thaliana',
-              '7227': 'D. melanogaster',
-              '4896': 'S. pombe',
-              '4932': 'S. cerevisiae'}
+taxon_dict = {	'9606': 'H. sapiens',
+				'7955': 'D. rerio',
+				'6239': 'C. elegans',
+				'3702': 'A. thaliana',
+				'7227': 'D. melanogaster',
+				'4896': 'S. pombe',
+				'4932': 'S. cerevisiae' }
 
 # Taxon column names
-taxon_dict_go = {'9606': 'H. sapiens Hit',
-                 '7955': 'D. rerio Hit',
-                 '6239': 'C. elegans Hit',
-                 '3702': 'A. thaliana Hit',
-                 '7227': 'D. melanogaster Hit',
-                 '4896': 'S. pombe Hit',
-                 '4932': 'S. cerevisiae Hit'
-                 }
+taxon_dict_go = {	'9606': 'H. sapiens Hit',
+					'7955': 'D. rerio Hit',
+					'6239': 'C. elegans Hit',
+					'3702': 'A. thaliana Hit',
+					'7227': 'D. melanogaster Hit',
+					'4896': 'S. pombe Hit',
+					'4932': 'S. cerevisiae Hit'
+					 }
